4310502dfa7fb4670dad66bba45f4302ac661da3
Henrik Uggla <henrik.uggla@kristianstad.se>
2020-03-20 14:31:24 +0100

    Update run.sh

--- agent/run.sh
+++ agent/run.sh
@@ -353,17 +353,17 @@ inspect()
 {
     print_token
 
-    if docker info 2>/dev/null | grep -i boot2docker >/dev/null 2>&1; then
+ #   if docker info 2>/dev/null | grep -i boot2docker >/dev/null 2>&1; then
         info env "CATTLE_BOOT2DOCKER=true"
         info env "CATTLE_VAR_LIB_WRITABLE=false"
-    else
-        info env "CATTLE_BOOT2DOCKER=false"
-        if mkdir -p /var/lib/rancher/state >/dev/null 2>&1; then
-            info env "CATTLE_VAR_LIB_WRITABLE=true"
-        else
-            info env "CATTLE_VAR_LIB_WRITABLE=false"
-        fi
-    fi
+  #  else
+   #     info env "CATTLE_BOOT2DOCKER=false"
+    #    if mkdir -p /var/lib/rancher/state >/dev/null 2>&1; then
+     #       info env "CATTLE_VAR_LIB_WRITABLE=true"
+     #   else
+     #       info env "CATTLE_VAR_LIB_WRITABLE=false"
+     #  fi
+  #  fi
 }
 
 setup_env()
